b0e4465bd40719c69011715b1c952e410fc1659e
VHThanh179 <thanhvh1709@gmail.com>
2021-09-29 21:43:10 +0700

    create Model User and Cus

---
+++ Share/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime DoB { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Share/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.Models
+{
+    public enum Role
+    {
+        [Display(Name = "Quản lý")]
+        Manager = 1,
+        [Display(Name = "Nhân viên")]
+        Staff = 2
+    }
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime DoB { get; set; }
+        public Role Roles { get; set; }
+        public bool Status { get; set; }
+        public string Password { get; set; }
+
+    }
+}
