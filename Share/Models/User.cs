38703725f3230d497295a545f333288a5247e4ee
Van <vannv0820@gmail.com>
2021-10-06 21:04:07 +0700

    VoucherSvc

--- Share/Models/User.cs
+++ Share/Models/User.cs
@@ -21,17 +21,34 @@ namespace Share.Models
         public int UserId { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
+        [Display(Name = "Tài khoản"), Required(ErrorMessage = "Mời nhập tài khoản")]
         public string UserName { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
+        [Display(Name = "Họ tên"), Required(ErrorMessage = "Mời nhập họ tên")]
         public string FullName { get; set; }
 
         [Column(TypeName = "varchar(50)"), MaxLength(50)]
+        [DataType(DataType.EmailAddress), Required(ErrorMessage = "Mời nhập Email")]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Email không hợp lệ")]
         public string Email { get; set; }
-        public DateTime DoB { get; set; }
-        public Role Roles { get; set; }
-        public bool Status { get; set; }
-        public string Password { get; set; }
 
+        [Column(TypeName = "date")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Ngày sinh")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DoB { get; set; }
+
+        [Display(Name = "Vai trò")]
+        [Required(ErrorMessage = "Mời chọn chức vụ"), Range(1, int.MaxValue, ErrorMessage = "Vui lòng chọn chức vụ")]
+        public Role Roles { get; set; }
+
+        [Display(Name = "Trạng thái hoạt động")]
+        public bool Status { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        [Column(TypeName = "varchar(50)"), MaxLength(50)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
