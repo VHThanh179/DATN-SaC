27d089a52a28b1a8b0aefe96a1e022cda8a457fe
VHThanh179 <thanhvh1709@gmail.com>
2021-10-07 12:46:36 +0700

    interface and service of customer, user

---
+++ Share/Interfaces/ICustomerSvc.cs
@@ -0,0 +1,19 @@
+﻿using Share.Models;
+using Share.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.Interfaces
+{
+    public interface ICustomerSvc
+    {
+        Task<List<Customer>> GetAllCustomerAsync();
+        Task<Customer> GetCustomerAsync(int id);
+        Task<int> AddCustomerAsync(Customer customer);
+        Task<int> EditCustomerAsync(int id, Customer customer);
+        Task<Customer> LoginAsync(ViewWebLogin login);
+    }
+}
---
+++ Share/Models/ViewModels/ViewWebLogin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.Models.ViewModels
+{
+    public class ViewWebLogin
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
