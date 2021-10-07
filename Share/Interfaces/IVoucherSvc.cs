d21a9931e3f12ed8e95baff381ed5e81f8431ac4
Van <vannv0820@gmail.com>
2021-10-07 17:38:13 +0700

    voucher service

---
+++ Share/Interfaces/IVoucherSvc.cs
@@ -0,0 +1,17 @@
+﻿using Share.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.Interfaces
+{
+    public interface IVoucherSvc
+    {
+        Task<List<Voucher>> GetVouchersAsync();
+        Task<Voucher> GetVoucherAsync(int id);
+        Task<int> AddVoucherAsync(Voucher voucher);
+        Task<int> EditVoucherAsync(int id, Voucher voucher);
+    }
+}
--- Share/Services/VoucherSvc.cs
+++ Share/Services/VoucherSvc.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Share.Interfaces;
+using Share.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,58 @@ using System.Threading.Tasks;
 
 namespace Share.Services
 {
-    class VoucherSvc
+    public class VoucherSvc : IVoucherSvc
     {
+        protected DataContext _context;
+        public VoucherSvc(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<Voucher> GetVoucherAsync(int id)
+        {
+            var voucher = await _context.Vouchers.FindAsync(id);
+            return voucher;
+        }
+
+        public async Task<List<Voucher>> GetVouchersAsync()
+        {
+            var list = await _context.Vouchers.ToListAsync();
+            return list;
+        }
+        public async Task<int> AddVoucherAsync(Voucher voucher)
+        {
+            int value = 0;
+            try
+            {
+                _context.Add(voucher);
+                await _context.SaveChangesAsync();
+                value = voucher.VoucherId;
+            }
+            catch (Exception){ value = 0; }
+            return value;
+        }
+
+        public async Task<int> EditVoucherAsync(int id, Voucher voucher)
+        {
+            int value = 0;
+            try
+            {
+                Voucher _voucher = null;
+                _voucher = _context.Vouchers.Find(id);
+
+                _voucher.VoucherCode = voucher.VoucherCode;
+                _voucher.VoucherQuantity = voucher.VoucherQuantity;
+                _voucher.StartDate = voucher.StartDate;
+                _voucher.EndDate = voucher.EndDate;
+                _voucher.CategoryDiscount = voucher.CategoryDiscount;
+                _voucher.Value = voucher.Value;
+
+                _context.Update(_voucher);
+                await _context.SaveChangesAsync();
+                value = voucher.VoucherId;
+            }
+            catch (Exception){ value = 0; }
+            return value;
+        }
     }
 }
