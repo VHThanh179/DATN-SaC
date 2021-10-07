5aac7382360c391a36fcea37b7b9ed30867543c3
VHThanh179 <thanhvh1709@gmail.com>
2021-10-07 21:40:26 +0700

    Edit IUserSvc

--- Share/Interfaces/IUserSvc.cs
+++ Share/Interfaces/IUserSvc.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Share.Models;
+using Share.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace Share.Interfaces
 {
     public interface IUserSvc
     {
+        Task<List<User>> GetAllUserAsync();
+        Task<User> GetUserAsync(int id);
+        Task<int> AddUserAsync(User user);
+        Task<int> EditUserAsync(int id, User user);
+        Task<User> LoginAsync(ViewLogin login);
     }
 }
