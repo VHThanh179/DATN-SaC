88d6595e44376f8047159c4977ad294635b7f03f
VHThanh179 <thanhvh1709@gmail.com>
2021-10-20 12:21:30 +0700

    Token and Cart controller

---
+++ Share/Models/ViewModels/ViewCart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.Models.ViewModels
+{
+    public class Cart
+    {
+        public int CustomerId { get; set; }
+        public List<CartItem> ListViewCart { get; set; } = new List<CartItem>();
+        public float Total { get; set; }
+    }
+
+    public class CartItem
+    {
+        public Product product { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+    }
+
+    public class ViewCart
+    {
+        public Product product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
