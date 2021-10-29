65494f7ad18993a90486a7807add0014f7ce48da
VHThanh179 <thanhvh1709@gmail.com>
2021-10-29 11:37:37 +0700

    Merge to Thanh Module

--- Server/obj/Debug/net5.0/RazorDeclaration/Pages/Customers/CustomerDialog.razor.g.cs
+++ Server/obj/Debug/net5.0/RazorDeclaration/Pages/Customers/CustomerDialog.razor.g.cs
@@ -13,77 +13,77 @@ namespace Server.Pages.Customers
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+#line 1 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+#line 2 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+#line 3 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+#line 4 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+#line 5 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+#line 6 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+#line 7 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+#line 8 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+#line 9 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
 using Server;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+#line 10 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
 using Server.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\Pages\Customers\CustomerDialog.razor"
+#line 2 "D:\DATN\Project\SaCBackpack\Server\Pages\Customers\CustomerDialog.razor"
 using Share.Models;
 
 #line default
@@ -98,7 +98,7 @@ using Share.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 59 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\Pages\Customers\CustomerDialog.razor"
+#line 59 "D:\DATN\Project\SaCBackpack\Server\Pages\Customers\CustomerDialog.razor"
        
     [Parameter]
     public string id { get; set; }
