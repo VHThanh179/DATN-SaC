14da3bcdebb3c1a01d23382d6ff0ab3901785445
Van <vannv0820@gmail.com>
2021-11-01 10:16:57 +0700

    layout cart, shipinfo, orderdetail, history

--- Client/obj/Debug/net5.0/RazorDeclaration/Pages/CartLayout.razor.g.cs
+++ Client/obj/Debug/net5.0/RazorDeclaration/Pages/CartLayout.razor.g.cs
@@ -13,133 +13,133 @@ namespace Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 1 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 2 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 3 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 4 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 5 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 6 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 7 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 8 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 10 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 11 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 12 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using Share.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 13 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using BlazorAnimate;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 14 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using Blazored;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 15 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using Blazored.Modal;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 16 "D:\DATN\Project\SaCBackpack\Client\_Imports.razor"
+#line 16 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\_Imports.razor"
 using Blazored.Modal.Services;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\DATN\Project\SaCBackpack\Client\Pages\CartLayout.razor"
+#line 2 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\Pages\CartLayout.razor"
 using System.Net;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\DATN\Project\SaCBackpack\Client\Pages\CartLayout.razor"
+#line 3 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\Pages\CartLayout.razor"
 using Share.Models.ViewModels;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\DATN\Project\SaCBackpack\Client\Pages\CartLayout.razor"
+#line 4 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\Pages\CartLayout.razor"
 using Newtonsoft.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\DATN\Project\SaCBackpack\Client\Pages\CartLayout.razor"
+#line 5 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\Pages\CartLayout.razor"
 using Share.Helpers;
 
 #line default
@@ -154,7 +154,7 @@ using Share.Helpers;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 184 "D:\DATN\Project\SaCBackpack\Client\Pages\CartLayout.razor"
+#line 224 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Client\Pages\CartLayout.razor"
        
     private string emailAddress;
     public Cart orderCart;
