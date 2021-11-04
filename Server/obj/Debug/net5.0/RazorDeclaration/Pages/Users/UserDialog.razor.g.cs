0b8cf655da90eec11dfa1e0c37041b8d4194e8ff
Van <vannv0820@gmail.com>
2021-11-04 17:48:55 +0700

    modify product (page -> modal), add 2 page changepass, layout footer

--- Server/obj/Debug/net5.0/RazorDeclaration/Pages/Users/UserDialog.razor.g.cs
+++ Server/obj/Debug/net5.0/RazorDeclaration/Pages/Users/UserDialog.razor.g.cs
@@ -13,84 +13,105 @@ namespace Server.Pages.Users
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 1 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 2 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 3 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 4 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 5 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 6 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 7 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 8 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 9 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Server;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 10 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Server.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 11 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Syncfusion.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\DATN\Project\SaCBackpack\Server\Pages\Users\UserDialog.razor"
+#line 12 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+using Blazored;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 13 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+using Blazored.Modal;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 14 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
+using Blazored.Modal.Services;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\Pages\Users\UserDialog.razor"
 using Share.Models;
 
 #line default
@@ -105,7 +126,7 @@ using Share.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 85 "D:\DATN\Project\SaCBackpack\Server\Pages\Users\UserDialog.razor"
+#line 85 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\Pages\Users\UserDialog.razor"
        
     [Parameter]
     public string id { get; set; }
