f60f522d84eab7c55d1f2328250f4e3705e2539e
Van <vannv0820@gmail.com>
2021-11-08 22:06:10 +0700

    cart layout - logout GG - changepass

--- Server/obj/Debug/net5.0/RazorDeclaration/Pages/ChangePassword.razor.g.cs
+++ Server/obj/Debug/net5.0/RazorDeclaration/Pages/ChangePassword.razor.g.cs
@@ -13,118 +13,118 @@ namespace Server.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 1 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 2 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 3 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 4 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 5 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 6 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 7 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 8 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 9 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Server;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 10 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Server.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 11 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Syncfusion.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 12 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Blazored;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 13 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Blazored.Modal;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 14 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Blazored.Modal.Services;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\DATN\Project\SaCBackpack\Server\Pages\ChangePassword.razor"
+#line 6 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\Pages\ChangePassword.razor"
 using Share.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\DATN\Project\SaCBackpack\Server\Pages\ChangePassword.razor"
+#line 7 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\Pages\ChangePassword.razor"
 using System.Security.Claims;
 
 #line default
 #line hidden
 #nullable disable
-    [Microsoft.AspNetCore.Components.LayoutAttribute(typeof(LoginLayout))]
+    [Microsoft.AspNetCore.Components.LayoutAttribute(typeof(MainLayout))]
     [Microsoft.AspNetCore.Components.RouteAttribute("/changepass")]
     public partial class ChangePassword : Microsoft.AspNetCore.Components.ComponentBase
     {
@@ -134,7 +134,7 @@ using System.Security.Claims;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 41 "D:\DATN\Project\SaCBackpack\Server\Pages\ChangePassword.razor"
+#line 41 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\Pages\ChangePassword.razor"
        
     public string oldPass, newPass, confirmPass;
 
@@ -143,7 +143,7 @@ using System.Security.Claims;
     User user = new User();
     protected override void OnInitialized()
     {
-        id = AuthStat.Result.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        id = AuthStat.Result.User.FindFirst(ClaimTypes.NameIdentifier).Value;
         user = _userService.GetUser(int.Parse(id));
     }
 
@@ -158,9 +158,11 @@ using System.Security.Claims;
                     user.Password = _encodeHelper.Encode(newPass);
                     user.ConfirmPass = user.Password;
                     _userService.EditUser(int.Parse(id), user);
+                    navigationManager.NavigateTo("/", true);
                 }
             }
         }
+
     }
 
 #line default
--- Server/obj/Debug/net5.0/RazorDeclaration/Pages/ShipInfoes/ShipInfoDialog.razor.g.cs
+++ Server/obj/Debug/net5.0/RazorDeclaration/Pages/ShipInfoes/ShipInfoDialog.razor.g.cs
@@ -13,119 +13,119 @@ namespace Server.Pages.ShipInfoes
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 1 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 2 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 3 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 4 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 5 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 6 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 7 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 8 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 9 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Server;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 10 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Server.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 11 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Syncfusion.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 12 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Blazored;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 13 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Blazored.Modal;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "D:\DATN\Project\SaCBackpack\Server\_Imports.razor"
+#line 14 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\_Imports.razor"
 using Blazored.Modal.Services;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\DATN\Project\SaCBackpack\Server\Pages\ShipInfoes\ShipInfoDialog.razor"
+#line 2 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\Pages\ShipInfoes\ShipInfoDialog.razor"
 using Share.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\DATN\Project\SaCBackpack\Server\Pages\ShipInfoes\ShipInfoDialog.razor"
+#line 3 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\Pages\ShipInfoes\ShipInfoDialog.razor"
 using System.IO;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\DATN\Project\SaCBackpack\Server\Pages\ShipInfoes\ShipInfoDialog.razor"
+#line 4 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\Pages\ShipInfoes\ShipInfoDialog.razor"
 using Microsoft.AspNetCore.Hosting;
 
 #line default
@@ -141,7 +141,7 @@ using Microsoft.AspNetCore.Hosting;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 81 "D:\DATN\Project\SaCBackpack\Server\Pages\ShipInfoes\ShipInfoDialog.razor"
+#line 81 "C:\Users\Navteiv\Desktop\DATN\DATN-SaC\Server\Pages\ShipInfoes\ShipInfoDialog.razor"
        
     [Parameter]
     public string id { get; set; }
